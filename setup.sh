fb421ac63ce3d0ab58e2e0e15a34614d5cabcf8a
alexfedosienko <alexfedosienko@gmail.com>
2023-01-09 14:04:40 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,4 +11,4 @@ curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compo
 chmod +x /usr/local/bin/docker-compose
 
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.9.3
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:
