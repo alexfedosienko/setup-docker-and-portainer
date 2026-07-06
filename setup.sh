7ea16a52f9dd5a73794e01f5250ae02877e30dc0
Alexander Fedosienko <alexfedosienko@gmail.com>
2026-07-06 14:49:14 +0500

    fix: исправил ссылки для установки

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/alexfedosienko/setup-docker-and-portainer/docker/setup.sh -O setup-docker.sh && sh setup-docker.sh
-wget https://raw.githubusercontent.com/alexfedosienko/setup-docker-and-portainer/portainer/setup.sh -O setup-portainer.sh && sh setup-portainer.sh
+wget https://raw.githubusercontent.com/alexfedosienko/setup-docker-and-portainer/refs/heads/main/setup-docker.sh -O setup-docker.sh && sh setup-docker.sh
+wget https://raw.githubusercontent.com/alexfedosienko/setup-docker-and-portainer/refs/heads/main/setup-portainer.sh -O setup-portainer.sh && sh setup-portainer.sh
